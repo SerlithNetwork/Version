fd75af35778ec1ff4ea6d9f7f1bfd70c83dc5122
Biquaternions <biquaternions@serlith.net>
2026-08-14 15:38:33 -0500

    Migrate service to jooq

--- server/src/main/java/net/serlith/version/server/service/ServerSoftwareService.java
+++ server/src/main/java/net/serlith/version/server/service/ServerSoftwareService.java
@@ -1,13 +1,15 @@
 package net.serlith.version.server.service;
 
 import lombok.RequiredArgsConstructor;
-import net.serlith.version.server.database.repository.ServerEntryRepository;
-import net.serlith.version.server.database.repository.ServerSoftwareRepository;
-import net.serlith.version.server.database.types.ServerEntry;
-import net.serlith.version.server.database.types.ServerSoftware;
+import net.serlith.version.server.schema.Tables;
+import net.serlith.version.server.schema.tables.records.VersionServerRecord;
+import net.serlith.version.server.schema.tables.records.VersionVersionRecord;
+import net.serlith.version.server.types.ServerVersionData;
 import net.serlith.version.server.types.SubmitBuildRequest;
+import org.jooq.DSLContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -17,33 +19,41 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ServerSoftwareService {
 
-    private final ServerSoftwareRepository softwareRepository;
-    private final ServerEntryRepository serverRepository;
+    private final DSLContext dsl;
 
     @Transactional
-    public Mono<ServerEntry> mergeServerBuild(final SubmitBuildRequest request) {
-        return this.softwareRepository.findByNameEqualsIgnoreCase(request.software())
+    public Mono<ServerVersionData> mergeServerBuild(final SubmitBuildRequest request) {
+
+        return Mono.from(
+                this.dsl.selectFrom(Tables.VERSION_SERVER)
+                        .where(Tables.VERSION_SERVER.NAME.equalIgnoreCase(request.software()))
+                )
                 .switchIfEmpty(Mono.error(new IllegalStateException(String.format("Requested project '%s' doesn't exist", request.software()))))
-                .flatMap(software -> Mono.zip(Mono.just(software), this.serverRepository.findAllBySoftware(software.id).collectList()))
+                .flatMap(server -> Mono.zip(Mono.just(server), Flux.from(
+                        this.dsl.selectFrom(Tables.VERSION_VERSION)
+                                .where(Tables.VERSION_VERSION.SOFTWARE_ID.eq(server.getId()))
+                ).collectList()))
                 .flatMap(tuple -> {
-                    final ServerSoftware software = tuple.getT1();
-                    final List<ServerEntry> entries = tuple.getT2();
-                    final Optional<ServerEntry> match = entries.stream().filter(entry -> entry.version.equalsIgnoreCase(request.version())).findFirst();
-                    final ServerEntry current = match.orElseGet(() -> {
-                        final ServerEntry entry = new ServerEntry();
-                        entry.software = software.id;
-                        entry.version = request.version();
-                        entry.build = 0;
-                        return entry;
-                    });
-
-                    if (current.build >= request.build()) {
-                        return Mono.error(new IllegalStateException(String.format("Submitted invalid build. Existing (%d) >= Requested (%d)", current.build, request.build())));
+                    final VersionServerRecord software = tuple.getT1();
+                    final List<VersionVersionRecord> entries = tuple.getT2();
+                    final Optional<VersionVersionRecord> match = entries.stream().filter(entry -> entry.getVersion().equalsIgnoreCase(request.version())).findFirst();
+                    final long current = match.stream().mapToLong(VersionVersionRecord::getBuild).findFirst().orElse(0L);
+                    if (current >= request.build()) {
+                        return Mono.error(new IllegalStateException(String.format("Submitted invalid build. Existing (%d) >= Requested (%d)", current, request.build())));
                     }
-                    current.build = request.build();
-
-                    return this.serverRepository.save(current);
-                });
+                    return Mono.zip(
+                            Mono.just(software),
+                            Mono.from(
+                                    this.dsl.insertInto(Tables.VERSION_VERSION)
+                                            .set(Tables.VERSION_VERSION.SOFTWARE_ID, software.getId())
+                                            .set(Tables.VERSION_VERSION.VERSION, request.version())
+                                            .set(Tables.VERSION_VERSION.BUILD, request.build())
+                                            .onDuplicateKeyUpdate()
+                                            .set(Tables.VERSION_VERSION.BUILD, request.build())
+                                            .returning()
+                            )
+                    );
+                }).map(ServerVersionData::from);
     }
 
 }
--- server/src/main/java/net/serlith/version/server/types/SubmitBuildRequest.java
+++ server/src/main/java/net/serlith/version/server/types/SubmitBuildRequest.java
@@ -3,6 +3,6 @@ package net.serlith.version.server.types;
 public record SubmitBuildRequest(
         String software,
         String version,
-        int build
+        long build
 ) {
 }
